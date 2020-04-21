a7a88557e956e8f9cf0d5bc5bb865a5b6b73767b
smr97 <saurabh.mraje@gmail.com>
2020-04-22 02:10:35 +0530

    moved the code inside algorithms

--- examples/merge.rs
+++ examples/merge.rs
@@ -1,39 +1,21 @@
 use rand::prelude::*;
-use rayon_try_fold::prelude::*;
-use rayon_try_fold::work;
+use rayon::prelude::*;
+use rayon_try_fold::adaptive_slice_merge;
 
-const SIZE: u64 = 10_000_000;
-
-struct Merger<'a> {
-    a: &'a [u64],
-    b: &'a [u64],
-    a_index: usize,
-    b_index: usize,
-    out: &'a mut [u64],
-    out_index: usize,
-}
+const SIZE: u64 = 1_000_000;
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let mut input = (0..SIZE).collect::<Vec<_>>();
+    let mut input = (0..SIZE / 5)
+        .cycle()
+        .take(SIZE as usize)
+        .collect::<Vec<_>>();
+    input.shuffle(&mut rng);
     let mid = input.len() / 2;
     let (left, right) = input.split_at_mut(mid);
-    left.shuffle(&mut rng);
-    right.shuffle(&mut rng);
+    left.par_sort();
+    right.par_sort();
     let mut output = (0..SIZE).map(|_| 0).collect::<Vec<_>>();
-    let merger = Merger {
-        a: left,
-        b: right,
-        a_index: 0,
-        b_index: 0,
-        out: output.as_mut_slice(),
-        out_index: 0,
-    };
-    work(
-        merger,
-        |m| m.out_index == m.out.len(),
-        |m| unimplemented!("divide"),
-        |m, s| unimplemented!("work by s"),
-    );
-    assert!(output.into_iter().eq(0..SIZE))
+    adaptive_slice_merge(left, right, output.as_mut_slice());
+    assert!(output.windows(2).all(|w| w[0] <= w[1]));
 }
