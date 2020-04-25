9f66c0d20d093dcb9bda0fd2a7995fc6181f9275
smr97 <saurabh.mraje@gmail.com>
2020-04-25 19:17:25 +0530

    added logging to the example

--- examples/par_sort.rs
+++ examples/par_sort.rs
@@ -1,6 +1,5 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
-use rayon::ThreadPoolBuilder;
 use rayon_try_fold::slice_par_sort;
 
 fn main() {
@@ -11,8 +10,25 @@ fn main() {
     let mut input = (0..1_000_000).collect::<Vec<u32>>();
     input.shuffle(&mut thread_rng());
     let solution = (0..1_000_000).collect::<Vec<u32>>();
-    tp.install(|| {
-        slice_par_sort(&mut input);
-    });
+    #[cfg(not(feature = "logs"))]
+    {
+        tp.install(|| {
+            slice_par_sort(&mut input);
+        });
+    }
+    #[cfg(feature = "logs")]
+    {
+        let tp = rayon_logs::ThreadPoolBuilder::new()
+            .num_threads(4)
+            .build()
+            .expect("Thread pool build failed");
+        let log = tp
+            .logging_install(|| {
+                slice_par_sort(&mut input);
+            })
+            .1;
+        log.save_svg("slice_par_sort.svg")
+            .expect("saving log failed");
+    }
     assert_eq!(input, solution);
 }
