1f0523e57179c56b72e61e79964a908f0e3cbdf4
smr97 <saurabh.mraje@gmail.com>
2020-06-27 23:00:16 +0530

    update gantt profiling to use fusion_task_cap

--- examples/sort_logprofile.rs
+++ examples/sort_logprofile.rs
@@ -6,16 +6,17 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        panic!("please enter problem_size num_threads as the two command line args for this");
+    if args.len() < 4 {
+        panic!("please enter problem_size num_threads task_cap as the three command line args for this");
     }
-    let PROBLEM_SIZE: u32 = args[1].parse().unwrap();
-    let NUM_THREADS: usize = args[2].parse().unwrap();
+    let problem_size: u32 = args[1].parse().unwrap();
+    let num_threads: usize = args[2].parse().unwrap();
+    let fusion_task_cap: isize = args[3].parse().unwrap();
 
     #[cfg(feature = "logs")]
     {
         let thread_pool = rayon_logs::ThreadPoolBuilder::new()
-            .num_threads(NUM_THREADS)
+            .num_threads(num_threads)
             .build()
             .expect("No thread pool for you");
         thread_pool
@@ -23,16 +24,19 @@ fn main() {
             .attach_algorithm_nodisplay_with_setup(
                 "slice par sort",
                 || {
-                    let mut input = (0..PROBLEM_SIZE).collect::<Vec<u32>>();
+                    let mut input = (0..problem_size).collect::<Vec<u32>>();
                     input.shuffle(&mut thread_rng());
                     input
                 },
                 |mut v| {
-                    slice_par_sort(&mut v);
+                    slice_par_sort(&mut v, fusion_task_cap);
                     v
                 },
             )
-            .generate_logs(format!("jccap_{}_{}.html", PROBLEM_SIZE, NUM_THREADS))
+            .generate_logs(format!(
+                "jccap_{}_{}_{}.html",
+                problem_size, num_threads, fusion_task_cap
+            ))
             .expect("No logs for you");
     }
     #[cfg(not(feature = "logs"))]
