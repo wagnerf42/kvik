47877c93a3c9e50cee31eb87837ec137bfcd9f44
smr97 <saurabh.mraje@gmail.com>
2020-07-08 21:44:17 +0530

    macronize range with types; The Step trait is currently nightly. Once it becomes stable, we can use it to abstract over any types that can be items of any Iterator

--- src/range.rs
+++ src/range.rs
@@ -1,52 +1,66 @@
 use crate::prelude::*;
 
-pub struct Iter {
-    range: std::ops::Range<u64>,
+pub struct Iter<T> {
+    range: std::ops::Range<T>,
 }
 
-impl IntoParallelIterator for std::ops::Range<u64> {
-    type Item = u64;
-    type Iter = Iter;
-    fn into_par_iter(self) -> Self::Iter {
-        Iter { range: self }
-    }
+macro_rules! implement_traits {
+    ($x: ty) => {
+        impl IntoParallelIterator for std::ops::Range<$x> {
+            type Item = $x;
+            type Iter = Iter<$x>;
+            fn into_par_iter(self) -> Self::Iter {
+                Iter { range: self }
+            }
+        }
+
+        impl ParallelIterator for Iter<$x> {
+            type Item = $x;
+            type Controlled = True;
+            type Enumerable = True;
+            fn with_producer<CB>(self, callback: CB) -> CB::Output
+            where
+                CB: ProducerCallback<Self::Item>,
+            {
+                callback.call(self.range)
+            }
+        }
+
+        impl Divisible for std::ops::Range<$x> {
+            type Controlled = True;
+            fn should_be_divided(&self) -> bool {
+                (self.end - self.start) >= 2
+            }
+            fn divide(self) -> (Self, Self) {
+                // We need to divide with the biggest half on the left, so instead of just doing
+                // (end - start) / 2, which in cases of an odd length will put the biggest
+                // half on the right, we need to make sure we take the ceiling of the value:
+                // this is done using (end - start) - ((end - start) / 2).
+                let index = (self.end - self.start) - ((self.end - self.start) / 2);
+                self.divide_at(index as usize)
+            }
+            fn divide_at(self, index: usize) -> (Self, Self) {
+                let mid = self.start + (index as $x);
+                (self.start..mid, mid..self.end)
+            }
+        }
+
+        impl Producer for std::ops::Range<$x> {
+            fn preview(&self, index: usize) -> Self::Item {
+                self.start + index as $x
+            }
+        }
+
+        impl PreviewableParallelIterator for Iter<$x> {}
+    };
 }
 
-impl ParallelIterator for Iter {
-    type Item = u64;
-    type Controlled = True;
-    type Enumerable = True;
-    fn with_producer<CB>(self, callback: CB) -> CB::Output
-    where
-        CB: ProducerCallback<Self::Item>,
-    {
-        callback.call(self.range)
-    }
-}
-
-impl Divisible for std::ops::Range<u64> {
-    type Controlled = True;
-    fn should_be_divided(&self) -> bool {
-        (self.end - self.start) >= 2
-    }
-    fn divide(self) -> (Self, Self) {
-        // We need to divide with the biggest half on the left, so instead of just doing
-        // (end - start) / 2, which in cases of an odd length will put the biggest
-        // half on the right, we need to make sure we take the ceiling of the value:
-        // this is done using (end - start) - ((end - start) / 2).
-        let index = (self.end - self.start) - ((self.end - self.start)/ 2);
-        self.divide_at(index as usize)
-    }
-    fn divide_at(self, index: usize) -> (Self, Self) {
-        let mid = self.start + (index as u64);
-        (self.start..mid, mid..self.end)
-    }
-}
-
-impl Producer for std::ops::Range<u64> {
-    fn preview(&self, index: usize) -> Self::Item {
-        self.start + index as u64
-    }
-}
-
-impl PreviewableParallelIterator for Iter {}
+implement_traits!(i16);
+implement_traits!(u16);
+implement_traits!(i32);
+implement_traits!(isize);
+implement_traits!(u8);
+implement_traits!(usize);
+implement_traits!(i8);
+implement_traits!(u32);
+implement_traits!(u64);
